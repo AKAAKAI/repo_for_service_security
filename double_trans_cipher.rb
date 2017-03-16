3a331dcb329c80880ad03ba058403d96aaac3d9b
AKAAKAI <twnoken@gmail.com>
2017-03-16 23:47:37 +0800

    update

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,35 +1,46 @@
 # Double transposition cipher
 module DoubleTranspositionCipher
   def self.encrypt(document, key)
-    chars_a = document.to_s.chars
-    m_size = Math.sqrt(chars_a.length).ceil
-    matrix = chars_a.each_slice(m_size).to_a
-    col_order = (0..m_size - 1).to_a.shuffle(random: Random.new(key))
+    m_size = ceil(document)
+    matrix = document.to_s.chars.each_slice(m_size).to_a
     row_order = (0..matrix.length - 1).to_a.shuffle(random: Random.new(key))
-    matrix_rows = []
-    row_order.each { |e| matrix_rows << matrix[e] }
-    matrix_columns = matrix_rows.map(&:dup)
-    matrix_columns.each_with_index do |row, index|
-      col_order.each_with_index do |e, i|
-        chr = matrix_rows[index][e]
-        row[i] = chr.nil? ? ' ' : chr
-      end
-    end
+    matrix_columns = matrix_columns_en(document, key, row_order, matrix)
     matrix_columns.join.to_s
   end
 
   def self.decrypt(ciphertext, key)
-    chars_a = ciphertext.to_s.chars
-    m_size = Math.sqrt(chars_a.length).ceil
-    matrix = chars_a.each_slice(m_size).to_a
+    m_size = ceil(ciphertext)
+    matrix = ciphertext.to_s.chars.each_slice(m_size).to_a
     col_order = (0..m_size - 1).to_a.shuffle(random: Random.new(key))
     row_order = (0..matrix.length - 1).to_a.shuffle(random: Random.new(key))
+    matrix_columns_de(matrix, col_order, row_order).join.to_s.strip
+  end
+
+  def self.ceil(text)
+    Math.sqrt(text.to_s.chars.length).ceil
+  end
+
+  def self.matrix_columns_de(matrix, col_order, row_order)
     matrix_col = matrix.map(&:dup)
     matrix_rows = matrix.map(&:dup)
     matrix_col.each_with_index do |row, index|
       col_order.each_with_index { |e, i| row[e] = matrix_rows[index][i] }
     end
     row_order.each_with_index { |e, i| matrix_rows[e] = matrix_col[i] }
-    matrix_rows.join.to_s.strip
+    matrix_rows
   end
+
+  def self.matrix_columns_en(document, key, row_order, matrix)
+    m_r = []
+    row_order.each { |e| m_r << matrix[e] }
+    col_order = (0..ceil(document) - 1).to_a.shuffle(random: Random.new(key))
+    m_c = m_r.map(&:dup)
+    m_c.each_with_index do |row, index|
+      col_order.each_with_index do |e, i|
+        row[i] = m_r[index][e].nil? ? ' ' : m_r[index][e]
+      end
+    end
+    m_c
+  end
+  
 end
