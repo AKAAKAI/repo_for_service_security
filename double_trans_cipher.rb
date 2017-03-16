051d7e50140d1f0806856c79c294fdfd0ce758fb
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-03-16 10:18:48 +0800

    Double transposition cipher

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,15 +1,48 @@
+require 'json'
 module DoubleTranspositionCipher
+  @col_order = []
+  @row_order = []
   def self.encrypt(document, key)
     # TODO: FILL THIS IN!
     ## Suggested steps for double transposition cipher
     # 1. find number of rows/cols such that matrix is almost square
-    # 2. break plaintext into evenly sized blocks
+    # 2. break plaintext into evenly sized blocksr
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
+    
+    key = 3
+    chars_a = document.to_s.chars
+    m_size = Math.sqrt(chars_a.length).ceil
+    matrix = chars_a.each_slice(m_size).to_a
+    print matrix
+    @col_order = (0..m_size-1).to_a.shuffle(random: Random.new(key))
+    @row_order = (0..matrix.length-1).to_a.shuffle(random: Random.new(key))
+    matrix_rows_shuffle = []
+    @row_order.each{ |e| matrix_rows_shuffle << matrix[e] }
+    matrix_columns_shuffle = matrix_rows_shuffle.map(&:dup)
+    matrix_columns_shuffle.each_with_index do |row, index| 
+      @col_order.each_with_index do |e, i| 
+        matrix_columns_shuffle[index][i] = matrix_rows_shuffle[index][e]
+      end
+    end
+    matrix_columns_shuffle.join()
   end
 
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
+    #ciphertext = "tkcaatatnadw"
+    chars_a = ciphertext.to_s.chars
+    m_size = @col_order.length
+    matrix = chars_a.each_slice(m_size).to_a
+    matrix_columns_ordered = matrix.map(&:dup)
+    matrix_rows_ordered = matrix.map(&:dup)
+    matrix_columns_ordered.each_with_index do |row, index| 
+      @col_order.each_with_index do |e, i| 
+        matrix_columns_ordered[index][e] = matrix_rows_ordered[index][i]
+      end
+    end
+    @row_order.each_with_index{ |e, i| matrix_rows_ordered[e] = matrix_columns_ordered[i] }
+    matrix_rows_ordered.join().to_s.force_encoding('utf-8')
   end
 end
